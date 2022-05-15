6833844508a70f88c5898625aba5b98fc84bfcac
cy <781449749@qq.com>
2022-05-15 20:27:28 +0800

    nnnn

---
+++ src/main/java/com/example/demo77777/Demo77777Application.java
@@ -0,0 +1,13 @@
+package com.example.demo77777;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo77777Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo77777Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo77777/Demo77777ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo77777;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo77777ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
